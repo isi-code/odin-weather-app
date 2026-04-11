0add8869cfcf801c56d046a004571bf962267b8a
isi-code <irvinjose.silva@gmail.com>
2026-04-11 00:04:43 -0700

    Add a class that returns a promise with the weather forecast info

--- index.js
+++ index.js
@@ -1,56 +1,69 @@
 import { createForm } from "./js/dom-form.js";
+import { WEATHER_API_KEY, GIF_API_KEY } from "./env.js";
 
-const API_KEY = "";
-const place = "Tijuana";
 const main = document.querySelector("main");
 
-/* Get weather data using .then()/.catch() syntax
-fetch(
-  `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${place}?key=${API_KEY}`,
-)
-  .then((response) => response.json())
-  .then((data) => {
-    console.log(data); // log the actual JSON here
-  })
-  .catch((err) => {
-    console.error(err);
-  });
-*/
+class WeatherForecast {
+  #weatherAPI;
 
-async function getWeatherData(place) {
-  try {
-    const getWeather = await fetch(
-      `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${place}?key=${API_KEY}`,
-    );
+  constructor(weatherAPI) {
+    this.#weatherAPI = weatherAPI;
+  }
 
-    const weatherData = await getWeather.json();
+  async getWeatherData(place) {
+    const weatherForecast = {};
 
-    const {
-      description,
-      currentConditions: { icon, conditions, datetime, temp, sunrise, sunset },
-    } = weatherData;
+    try {
+      const requestWeather = await fetch(
+        `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${place}?key=${this.#weatherAPI}`,
+      );
 
-    const currentWeather = {
-      description,
-      icon,
-      conditions,
-      datetime,
-      temp,
-      sunrise,
-      sunset,
-    };
+      const weatherData = await requestWeather.json();
 
-    console.log(currentWeather);
+      const { description } = weatherData;
 
-  } catch (error) {
-    console.error(error);
+      weatherForecast["currentWeatherLongDesc"] = description;
+
+      const fiveDayWeather = weatherData.days.slice(0, 5);
+
+      weatherForecast["fiveDayWeather"] = fiveDayWeather.map((day) => {
+        const {
+          icon,
+          description,
+          conditions,
+          datetime,
+          temp,
+          sunrise,
+          sunset,
+        } = day;
+
+        return {
+          icon,
+          description,
+          conditions,
+          datetime,
+          temp,
+          sunrise,
+          sunset,
+        };
+      });
+    } catch (error) {
+      console.error(error);
+    }
+
+    return weatherForecast;
   }
 }
 
+
+
+const weatherForecast = new WeatherForecast(WEATHER_API_KEY);
+
+// Display to DOM
 main.append(createForm());
 
 document.forms[0].addEventListener("submit", (e) => {
   e.preventDefault();
   const place = document.forms[0].place.value;
-  getWeatherData(place);
+  weatherForecast.getWeatherData(place).then(v => { console.log(v) });
 });
