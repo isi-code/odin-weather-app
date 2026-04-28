fbf37987bfa33d8de990a55197d8988af746f6b7
isi-code <irvinjose.silva@gmail.com>
2026-04-27 20:48:45 -0700

    Add WeatherApp class
    I created this class to improve code organization.

--- index.js
+++ index.js
@@ -1,13 +1,39 @@
 import { createForm } from "./js/dom-form.js"
 import { formSubmitted } from "./js/form-logic.js";
 
+class WeatherApp {
+    constructor(container){
+        this.container = this.#createContainers(container);
+        this.formContainer = this.container.querySelector(".formSection");
+        this.weatherCont = this.container.querySelector(".weatherForecastSect");
+        this.#consultWeatherForm();
+    }
+
+    #createContainers(container){
+        const formSection = document.createElement("section");
+        const weatherSection = document.createElement("section");
+
+        formSection.className = "formSection";
+        weatherSection.className = "weatherForecastSect";
+
+        container.append(formSection, weatherSection);
+        return container
+    }
+
+    #consultWeatherForm(){
+        const weatherForm = createForm();
+
+        weatherForm.addEventListener("submit", (e) => {
+            e.preventDefault();
+            const currWeatherCard = formSubmitted(weatherForm.place.value);
+            this.weatherCont.append(currWeatherCard);
+        });
+
+        this.formContainer.append(weatherForm);        
+        return this.container
+    }
+
+}
+
 const main = document.querySelector("main");
-
-// Display form to request location to DOM
-const weatherForm = createForm();
-main.append(weatherForm);
-
-weatherForm.addEventListener("submit", (e) => {
-    e.preventDefault();
-    const currWeatherCard = formSubmitted(weatherForm.place.value);
-});
+const weatherApp = new WeatherApp(main);
