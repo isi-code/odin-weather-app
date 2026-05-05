fbe662c892596811677bd24cafda54430c068f83
isi-code <irvinjose.silva@gmail.com>
2026-05-05 07:48:05 -0700

    refact: Update form structure and generation logic
    I added radio buttons to select temperature units. The html tag generation logic was updated to create tags contained in other tags.

--- js/dom-form.js
+++ js/dom-form.js
@@ -1,24 +1,83 @@
-export function createForm() {
-  const form = document.createElement("form");
-
-  const formDOM = {
-    placeLabel: { tag: "label", textContent: "Location", htmlFor: "place" },
-    placeInput: {
-      tag: "input",
-      type: "text",
-      placeholder: "Example: Tijuana",
-      name: "place",
-      id: "place",
+const formDOM = {
+  form: [
+    { label: { textContent: "Location", htmlFor: "place" } },
+    {
+      input: {
+        type: "text",
+        placeholder: "Example: Tijuana",
+        name: "place",
+        id: "place",
+      },
     },
-    button: { tag: "button", textContent: "Search Weather", type: "submit" },
-  };
+    {
+      fieldset: [
+        { legend: { textContent: "Temperature Scale" } },
+        {
+          div: {
+            content: [
+              {
+                input: {
+                  type: "radio",
+                  id: "celsius",
+                  name: "tempUnit",
+                  value: "metric",
+                  checked: true,
+                },
+              },
+              {
+                label: {
+                  textContent: "Celsius (C)",
+                  htmlFor: "celsius",
+                },
+              },
+              {
+                input: {
+                  type: "radio",
+                  id: "faren",
+                  name: "tempUnit",
+                  value: "us",
+                },
+              },
+              { label: { textContent: "Farenheit (F)", htmlFor: "faren" } },
+            ],
+            attr: {
+              className: "tempMetric",
+            },
+          },
+        },
+      ],
+    },
+    { button: { textContent: "Search Weather", type: "submit" } },
+  ],
+};
 
-  for (const key in formDOM) {
-    const elem = document.createElement(formDOM[key].tag);
-    Object.assign(elem, formDOM[key]);
-    delete elem.tag;
+export function createForm(obj = formDOM) {
+  for (const [key, value] of Object.entries(obj)) {
+    const elem = document.createElement(key);
 
-    form.append(elem);
+    if (Array.isArray(value)) {
+      const content = value.map((el) => {
+        const [[k, v]] = Object.entries(el);
+
+        if (Array.isArray(v)) return createForm(el);
+        else if (
+          typeof v === "object" &&
+          v !== null &&
+          ("content" in v || "attr" in v)
+        ) {
+          const contents = { [k]: v.content };
+          const newTag = createForm(contents);
+          if (v.attr) Object.assign(newTag, v.attr);
+          return newTag;
+        } else {
+          const childElem = document.createElement(k);
+          Object.assign(childElem, v);
+          return childElem;
+        }
+      });
+
+      elem.append(...content);
+    }
+    return elem;
   }
-  return form;
 }
