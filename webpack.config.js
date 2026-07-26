b896d3bde73c682cc17c3022b0ad4a7d88b9738e
isi-code <irvinjose.silva@gmail.com>
2026-07-25 17:56:24 -0700

    refactor! : Create class that handles all DOM handling
    I removed uneeded files and rename template.html file to index.html to keep consistency

--- webpack.config.js
+++ webpack.config.js
@@ -27,7 +27,7 @@ export default {
     },
     plugins:[
         new HtmlWebpackPlugin({
-            template:"./src/template.html",
+            template:"./src/index.html",
         })
     ]
 }
