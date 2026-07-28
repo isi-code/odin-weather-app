7364aa89412bf926207cfe1e12adc493ae4ceb7c
isi-code <irvinjose.silva@gmail.com>
2026-07-28 13:45:27 -0700

    Add eslint and prettier libraries to the project
    These two libraries helps to keep a consistent coding style and prevent errors.
    Lastly, I ran `prettier --write .` to keep consistent code styling in all files.
    
    _pnpm package manager version updated_

--- src/js/weather-forecast-data.js
+++ src/js/weather-forecast-data.js
@@ -1,4 +1,4 @@
-import { format } from "date-fns";
+import { format } from 'date-fns';
 
 export class WeatherForecast {
   #weatherAPI;
@@ -10,45 +10,46 @@ export class WeatherForecast {
   async getWeatherData(place, tempUnit) {
     try {
       const requestWeather = await fetch(
-        `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${place}?unitGroup=${tempUnit}&key=${this.#weatherAPI}`,
+        `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${place}?unitGroup=${tempUnit}&key=${this.#weatherAPI}`
       );
 
       const weatherData = await requestWeather.json();
-      
+
       /*
         This long code portion has the purpose of further processing the data obtained from the API
         and put it into an object where it is ready to use.
-      */      
-      return await Promise.all( weatherData.days.slice(0, 6).map(async (day) => {
-        const {
-          description,
-          conditions,
-          datetime,
-          temp,
-          icon,
-          feelslike,
-          tempmax,
-          tempmin,
-          sunrise,
-          sunset
-        } = day;
+      */
+      return await Promise.all(
+        weatherData.days.slice(0, 6).map(async (day) => {
+          const {
+            description,
+            //conditions,
+            datetime,
+            temp,
+            icon,
+            feelslike,
+            tempmax,
+            tempmin,
+            sunrise,
+            sunset,
+          } = day;
 
-        const tempUnitSymbol = tempUnit === 'metric' ? '°C': '°F';
-        const {default: iconUrl } = await import(`../img/icons/${icon}.svg`);
+          const tempUnitSymbol = tempUnit === 'metric' ? '°C' : '°F';
+          const { default: iconUrl } = await import(`../img/icons/${icon}.svg`);
 
-        return {
-          icon: iconUrl,
-          description,
-          fullDate: format(new Date(datetime), 'PPPP'),
-          temp: temp + tempUnitSymbol,
-          feelslike: `Feels like ${feelslike}${tempUnitSymbol}`,
-          tempmax: tempmax + tempUnitSymbol,
-          tempmin: tempmin + tempUnitSymbol,
-          sunrise,
-          sunset
-        }
-      }));
-
+          return {
+            icon: iconUrl,
+            description,
+            fullDate: format(new Date(datetime), 'PPPP'),
+            temp: temp + tempUnitSymbol,
+            feelslike: `Feels like ${feelslike}${tempUnitSymbol}`,
+            tempmax: tempmax + tempUnitSymbol,
+            tempmin: tempmin + tempUnitSymbol,
+            sunrise,
+            sunset,
+          };
+        })
+      );
     } catch (error) {
       console.error(error);
     }
--- webpack.config.js
+++ webpack.config.js
@@ -1,44 +1,49 @@
-import path from "node:path";
+import path from 'node:path';
 import webpack from 'webpack';
-import HtmlWebpackPlugin from "html-webpack-plugin";
-import { loadEnvFile } from 'node:process'
+import HtmlWebpackPlugin from 'html-webpack-plugin';
+import ESLintWebpackPlugin from 'eslint-webpack-plugin';
+import { loadEnvFile } from 'node:process';
 
 loadEnvFile();
 
-export default (env) =>{
-    return {
-        mode: env.NODE_ENV || 'development',
-        entry:"./src/index.js",
-        output: {
-            filename: "main.js",
-            path: path.resolve(import.meta.dirname, "dist"),
-            clean: true,
+export default (env) => {
+  return {
+    mode: env.NODE_ENV || 'development',
+    entry: './src/index.js',
+    output: {
+      filename: 'main.js',
+      path: path.resolve(import.meta.dirname, 'dist'),
+      clean: true,
+    },
+    module: {
+      rules: [
+        {
+          test: /\.css$/i,
+          use: ['style-loader', 'css-loader'],
         },
-        module: {
-            rules:[
-                { 
-                    test: /\.css$/i, 
-                    use: ['style-loader','css-loader'] 
-                },
-                {
-                    test:/\.html$/i,
-                    use: 'html-loader'
-                },
-                {
-                    test: /\.svg$/i,
-                    type: 'asset/resource'
-                }
-            ]
+        {
+          test: /\.html$/i,
+          use: 'html-loader',
         },
-        devtool: "source-map",
-        devServer: {
-            watchFiles: ["./src/index.html"],
+        {
+          test: /\.svg$/i,
+          type: 'asset/resource',
         },
-        plugins:[
-            new HtmlWebpackPlugin({
-                template:"./src/index.html",
-            }),
-            new webpack.EnvironmentPlugin(["WEATHER_API_KEY"])
-        ]
-    }
-}
+      ],
+    },
+    devtool: 'source-map',
+    devServer: {
+      watchFiles: ['./src/index.html'],
+    },
+    plugins: [
+      new HtmlWebpackPlugin({
+        template: './src/index.html',
+      }),
+      new webpack.EnvironmentPlugin(['WEATHER_API_KEY']),
+      new ESLintWebpackPlugin({
+        extensions: ['js', 'mjs', 'cjs'],
+        fix: true,
+      }),
+    ],
+  };
+};
